0a9200ce68b2c1b181515a3eedfd6bff103a02fd
rolivieri <roliv@us.ibm.com>
2016-03-21 22:56:17 -0500

    Added more test cases and new logic.

--- Tests/CFEnvironment/Tests.swift
+++ Tests/CFEnvironment/Tests.swift
@@ -46,13 +46,16 @@ class Tests : XCTestCase {
   }
 
   let options = "{ \"vcap\": { \"application\": { \"limits\": { \"mem\": 128, \"disk\": 1024, \"fds\": 16384 }, \"application_id\": \"e582416a-9771-453f-8df1-7b467f6d78e4\", \"application_version\": \"e5e029d1-4a1a-4004-9f79-655d550183fb\", \"application_name\": \"swift-test\", \"application_uris\": [ \"swift-test.mybluemix.net\" ], \"version\": \"e5e029d1-4a1a-4004-9f79-655d550183fb\", \"name\": \"swift-test\", \"space_name\": \"dev\", \"space_id\": \"b15eb0bb-cbf3-43b6-bfbc-f76d495981e5\", \"uris\": [ \"swift-test.mybluemix.net\" ], \"users\": null, \"instance_id\": \"7d4f24cfba06462ba23d68aaf1d7354a\", \"instance_index\": 0, \"host\": \"0.0.0.0\", \"port\": 61263, \"started_at\": \"2016-03-04 02:43:07 +0000\", \"started_at_timestamp\": 1457059387, \"start\": \"2016-03-04 02:43:07 +0000\", \"state_timestamp\": 1457059387 }, \"services\": { \"cloudantNoSQLDB\": [ { \"name\": \"Cloudant NoSQL DB-kd\", \"label\": \"cloudantNoSQLDB\", \"tags\": [ \"data_management\", \"ibm_created\", \"ibm_dedicated_public\" ], \"plan\": \"Shared\", \"credentials\": { \"username\": \"09ed7c8a-fae8-48ea-affa-0b44b2224ec0-bluemix\", \"password\": \"06c19ae06b1915d8a6649df5901eca85e885182421ffa9ef89e14bbc1b76efd4\", \"host\": \"09ed7c8a-fae8-48ea-affa-0b44b2224ec0-bluemix.cloudant.com\", \"port\": 443, \"url\": \"https://09ed7c8a-fae8-48ea-affa-0b44b2224ec0-bluemix:06c19ae06b1915d8a6649df5901eca85e885182421ffa9ef89e14bbc1b76efd4@09ed7c8a-fae8-48ea-affa-0b44b2224ec0-bluemix.cloudant.com\" } } ] } } }"
+  var jsonOptions: JSON = [:]
 
   override func setUp() {
     super.setUp()
+    jsonOptions = JSONUtils.convertStringToJSON(options) as JSON!
   }
 
   override func tearDown() {
     super.tearDown()
+    jsonOptions = [:]
   }
 
   func testConvertStringToJSON() {
@@ -87,40 +90,36 @@ class Tests : XCTestCase {
 
   func testGetApp() {
     do {
-      if let json = JSONUtils.convertStringToJSON(options) {
-        let appEnv = try CFEnvironment.getAppEnv(json)
-        //print(appEnv.app)
-        let app = appEnv.getApp()
-        //print("app: \(app)")
-        XCTAssertNotNil(app)
-        XCTAssertEqual(app.port, 61263, "Application port number should match.")
-        XCTAssertEqual(app.id, "e582416a-9771-453f-8df1-7b467f6d78e4", "Application ID value should match.")
-        XCTAssertEqual(app.version, "e5e029d1-4a1a-4004-9f79-655d550183fb", "Application version number should match.")
-        XCTAssertEqual(app.name, "swift-test", "App name should match.")
-        XCTAssertEqual(app.instanceId, "7d4f24cfba06462ba23d68aaf1d7354a", "Application instance ID value should match.")
-        XCTAssertEqual(app.instanceIndex, 0, "Application instance index value should match.")
-        XCTAssertEqual(app.spaceId, "b15eb0bb-cbf3-43b6-bfbc-f76d495981e5", "Application space ID value should match.")
-        let limits = app.limits
-        //print("limits: \(limits)")
-        XCTAssertNotNil(limits)
-        XCTAssertEqual(limits!.memory, 128, "Memory value should match.")
-        XCTAssertEqual(limits!.disk, 1024, "Disk value should match.")
-        XCTAssertEqual(limits!.fds, 16384, "FDS value should match.")
-        let uris = app.uris
-        XCTAssertNotNil(uris)
-        XCTAssertEqual(uris!.count, 1, "There should be only 1 uri in the uris array.")
-        XCTAssertEqual(uris![0], "swift-test.mybluemix.net", "URI value should match.")
-        XCTAssertEqual(app.name, "swift-test", "Application name should match.")
-        let startedAt: NSDate? = app.startedAt
-        XCTAssertNotNil(startedAt)
-        let dateUtils = DateUtils()
-        let startedAtStr = dateUtils.convertNSDateToString(startedAt)
-        XCTAssertEqual(startedAtStr, "2016-03-04 02:43:07 +0000", "Application startedAt date should match.")
-        XCTAssertNotNil(app.startedAtTs, "Application startedAt ts should not be nil.")
-        XCTAssertEqual(app.startedAtTs, 1457059387, "Application startedAt ts should match.")
-      } else {
-        XCTFail("Could not generate JSON object!")
-      }
+      let appEnv = try CFEnvironment.getAppEnv(jsonOptions)
+      //print(appEnv.app)
+      let app = appEnv.getApp()
+      //print("app: \(app)")
+      XCTAssertNotNil(app)
+      XCTAssertEqual(app.port, 61263, "Application port number should match.")
+      XCTAssertEqual(app.id, "e582416a-9771-453f-8df1-7b467f6d78e4", "Application ID value should match.")
+      XCTAssertEqual(app.version, "e5e029d1-4a1a-4004-9f79-655d550183fb", "Application version number should match.")
+      XCTAssertEqual(app.name, "swift-test", "App name should match.")
+      XCTAssertEqual(app.instanceId, "7d4f24cfba06462ba23d68aaf1d7354a", "Application instance ID value should match.")
+      XCTAssertEqual(app.instanceIndex, 0, "Application instance index value should match.")
+      XCTAssertEqual(app.spaceId, "b15eb0bb-cbf3-43b6-bfbc-f76d495981e5", "Application space ID value should match.")
+      let limits = app.limits
+      //print("limits: \(limits)")
+      XCTAssertNotNil(limits)
+      XCTAssertEqual(limits!.memory, 128, "Memory value should match.")
+      XCTAssertEqual(limits!.disk, 1024, "Disk value should match.")
+      XCTAssertEqual(limits!.fds, 16384, "FDS value should match.")
+      let uris = app.uris
+      XCTAssertNotNil(uris)
+      XCTAssertEqual(uris!.count, 1, "There should be only 1 uri in the uris array.")
+      XCTAssertEqual(uris![0], "swift-test.mybluemix.net", "URI value should match.")
+      XCTAssertEqual(app.name, "swift-test", "Application name should match.")
+      let startedAt: NSDate? = app.startedAt
+      XCTAssertNotNil(startedAt)
+      let dateUtils = DateUtils()
+      let startedAtStr = dateUtils.convertNSDateToString(startedAt)
+      XCTAssertEqual(startedAtStr, "2016-03-04 02:43:07 +0000", "Application startedAt date should match.")
+      XCTAssertNotNil(app.startedAtTs, "Application startedAt ts should not be nil.")
+      XCTAssertEqual(app.startedAtTs, 1457059387, "Application startedAt ts should match.")
     } catch let error as NSError {
       print("Error domain: \(error.domain)")
       print("Error code: \(error.code)")
@@ -130,23 +129,19 @@ class Tests : XCTestCase {
 
   func testGetServices() {
     do {
-      if let json = JSONUtils.convertStringToJSON(options) {
-        //print("json \(json)")
-        let appEnv = try CFEnvironment.getAppEnv(json)
-        //let servs = appEnv.services
-        //print("servs \(servs)")
-        let services = appEnv.getServices()
-        //print("services \(services)")
-        XCTAssertEqual(services.count, 1, "There should be only 1 service in the services dictionary.")
-        let name = "Cloudant NoSQL DB-kd"
-        if let service = services[name] {
-          XCTAssertEqual(service.name, name, "Key in dictionary and service name should match.")
-          verifyService(service)
-        } else {
-          XCTFail("A service object should have been found for '\(name)'.")
-        }
+      //print("json \(json)")
+      let appEnv = try CFEnvironment.getAppEnv(jsonOptions)
+      //let servs = appEnv.services
+      //print("servs \(servs)")
+      let services = appEnv.getServices()
+      //print("services \(services)")
+      XCTAssertEqual(services.count, 1, "There should be only 1 service in the services dictionary.")
+      let name = "Cloudant NoSQL DB-kd"
+      if let service = services[name] {
+        XCTAssertEqual(service.name, name, "Key in dictionary and service name should match.")
+        verifyService(service)
       } else {
-        XCTFail("Could not generate JSON object!")
+        XCTFail("A service object should have been found for '\(name)'.")
       }
     } catch let error as NSError {
       print("Error domain: \(error.domain)")
@@ -157,22 +152,20 @@ class Tests : XCTestCase {
 
   func testGetService() {
     do {
-      if let json = JSONUtils.convertStringToJSON(options) {
-        let appEnv = try CFEnvironment.getAppEnv(json)
-        let name = "Cloudant NoSQL DB-kd"
-        if let service = appEnv.getService(name) {
-          verifyService(service)
-        } else {
-          XCTFail("A service object should have been found for '\(name)'.")
-        }
-        let regex = "Cloudant NoSQL*"
-        if let service = appEnv.getService(regex) {
-          verifyService(service)
-        } else {
-          XCTFail("A service object should have been found for '\(regex)'.")
-        }
+      let appEnv = try CFEnvironment.getAppEnv(jsonOptions)
+      let name = "Cloudant NoSQL DB-kd"
+
+      if let service = appEnv.getService(name) {
+        verifyService(service)
       } else {
-        XCTFail("Could not generate JSON object!")
+        XCTFail("A service object should have been found for '\(name)'.")
+      }
+
+      let regex = "Cloudant NoSQL*"
+      if let service = appEnv.getService(regex) {
+        verifyService(service)
+      } else {
+        XCTFail("A service object should have been found for '\(regex)'.")
       }
     } catch let error as NSError {
       print("Error domain: \(error.domain)")
@@ -237,16 +230,12 @@ class Tests : XCTestCase {
   }
 
   private func verifyServiceURLWithOptions(name: String, replacements: String?, expectedServiceURL: String) throws {
-    if let json = JSONUtils.convertStringToJSON(options) {
-      let appEnv = try CFEnvironment.getAppEnv(json)
-      let substitutions = JSONUtils.convertStringToJSON(replacements)
-      if let serviceURL = appEnv.getServiceURL(name, replacements: substitutions) {
-          XCTAssertEqual(serviceURL, expectedServiceURL, "ServiceURL should match '\(expectedServiceURL)'.")
-      } else {
-        XCTFail("A serviceURL should have been returned!")
-      }
+    let appEnv = try CFEnvironment.getAppEnv(jsonOptions)
+    let substitutions = JSONUtils.convertStringToJSON(replacements)
+    if let serviceURL = appEnv.getServiceURL(name, replacements: substitutions) {
+        XCTAssertEqual(serviceURL, expectedServiceURL, "ServiceURL should match '\(expectedServiceURL)'.")
     } else {
-      XCTFail("Could not generate JSON object!")
+      XCTFail("A serviceURL should have been returned!")
     }
   }
 
